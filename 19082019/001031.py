3bffb67f0f58e3e2feb7ce8d1e3cd3412d393c9a
naxolorca <filorca@miuandes.cl>
2019-08-19 22:41:49 -0400

    commit 1

---
+++ 19082019/001031.py
@@ -0,0 +1,15 @@
+#Felipe Lorca
+
+#Condicionales
+
+#Creamos nuestra primera condicion
+c = 3
+d = 4
+
+if c < d: #usando un condicionador logico verificamos si se cumple o no
+	print "c is lees than d" #esto debe estar indentado
+
+else: #Usando else, se corre el codigo indentado sol si ninguna de las pruebas logicas anteriores se cumple
+	print "c is NOT less than d"
+
+print "outside the if block" # este print representa lo que no este indentado en los condicionales siempre se "correra"
